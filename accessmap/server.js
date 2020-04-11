7b343f275c166764007d27746dda74f9254e34af
tommlogan <tomlogan@umich.edu>
2020-04-11 04:06:22 +0000

    updating node to work

--- accessmap/server.js
+++ accessmap/server.js
@@ -68,10 +68,10 @@ app.use(function (err, req, res, next) {
 });
 
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8081);
 
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
 // app.listen(process.env.PORT);
-// app.listen(3000, function() { console.log('listening')});
+// app.listen(8081, function() { console.log('listening')});
